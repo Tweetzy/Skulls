7c26dcdc09ecaf70efe3618bbaac86238ff9cfcd
Kiran Hart <kiran.hart@live.com>
2022-04-21 12:50:28 -0400

    create view mode to know if the list should be a search, list, all, or favourite
    Took 28 seconds

---
+++ src/main/java/ca/tweetzy/skulls/api/enums/ViewMode.java
@@ -0,0 +1,16 @@
+package ca.tweetzy.skulls.api.enums;
+
+/**
+ * Date Created: April 21 2022
+ * Time Created: 12:09 p.m.
+ *
+ * @author Kiran Hart
+ */
+public enum ViewMode {
+
+	FAVOURITE,
+	ALL,
+	LIST,
+	SEARCH
+
+}
