0109dcd42c833ba2682840d084fa464592fdf518
Kiran Hart <kiran.hart@live.com>
2022-04-21 11:00:58 -0400

    automatically download new inserts
    Took 10 minutes

--- src/main/java/ca/tweetzy/skulls/manager/SkullManager.java
+++ src/main/java/ca/tweetzy/skulls/manager/SkullManager.java
@@ -21,10 +21,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -171,6 +168,21 @@ public final class SkullManager implements Manager {
 					this.histories.addAll(dlHistory);
 				});
 			} else {
+				// check for new inserts
+				final List<History> dlHistory = downloadHistories();
+				if (dlHistory.size() != this.histories.size()) {
+					final Iterator<History> historyIterator = dlHistory.iterator();
+					while (historyIterator.hasNext()) {
+						final History history = historyIterator.next();
+						if (!this.histories.contains(history)) continue;
+						historyIterator.remove();
+					}
+
+					Skulls.getDataManager().insertHistories(dlHistory);
+					Common.log("&aInserts were found, saving the following: &e" + dlHistory.stream().map(h -> String.valueOf(h.getID())).collect(Collectors.joining(", ")));
+					return;
+				}
+
 				Common.log("&aLoaded &e" + this.histories.size() + "&a history inserts");
 			}
 		});
