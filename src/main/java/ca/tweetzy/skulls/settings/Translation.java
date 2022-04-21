7a9165809ad9ea6254b48b48e00fae39150a255a
Kiran Hart <kiran.hart@live.com>
2022-04-21 12:50:00 -0400

    add translations for heads
    Took 8 seconds

--- src/main/java/ca/tweetzy/skulls/settings/Translation.java
+++ src/main/java/ca/tweetzy/skulls/settings/Translation.java
@@ -4,6 +4,7 @@ import ca.tweetzy.rose.utils.Replacer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -35,7 +36,34 @@ public enum Translation {
 	MONSTERS("categories.monsters", "Monsters"),
 	PLANTS("categories.plants", "Plants"),
 
+	// GUIS
+
 	GUI_MAIN_TITLE("gui.main.title", "&eSkulls"),
+	GUI_MAIN_ITEMS_CATEGORY_NAME("gui.main.items.category.name", "&e%category_name%"),
+	GUI_MAIN_ITEMS_CATEGORY_LORE("gui.main.items.category.lore", Arrays.asList(
+			"&8View heads by this category",
+			"&7Total Heads&F: &b%category_size%",
+			"",
+			"&e&lClick &8» &7To view category"
+	)),
+
+	GUI_SKULLS_LIST_TITLE_CATEGORY("gui.skulls list.title.category", "&eSkulls &8> &7%category_name%"),
+	GUI_SKULLS_LIST_TITLE_FAVOURITES("gui.skulls list.title.favourites", "&eSkulls &8> &7Favourites"),
+	GUI_SKULLS_LIST_TITLE_SEARCH("gui.skulls list.title.search", "&eSkulls &8> &7%search_phrase%"),
+	GUI_SKULLS_LIST_TITLE_ALL("gui.skulls list.title.all", "&eSkulls &8> &7All"),
+
+	GUI_SKULLS_LIST_ITEMS_SKULL_NAME("gui.skulls list.items.skull.name", "&e%skull_name%"),
+
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_ID("gui.skulls list.items.skull.lore.id", "&7Id: &e%skull_id%"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_TAGS("gui.skulls list.items.skull.lore.tags", "&7Tags: &e%skull_tags%"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_PRICE("gui.skulls list.items.skull.lore.price", "&7Cost: &a$%skull_price%"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_TAKE("gui.skulls list.items.skull.lore.take", "&e&lLeft Click &8» &7To take one"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_FAVOURITE("gui.skulls list.items.skull.lore.favourite", "&e&lRight Click &8» &7To favourite head"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_UN_FAVOURITE("gui.skulls list.items.skull.lore.un favourite", "&e&lRight Click &8» &7To unfavourite head"),
+
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_FAVOURITED("gui.skulls list.items.skull.lore.favourited", "&e&lFAVOURITED"),
+	GUI_SKULLS_LIST_ITEMS_SKULL_LORE_BLOCKED("gui.skulls list.items.skull.lore.blocked", "&c&oPurchase is blocked"),
+
 
 	;
 
