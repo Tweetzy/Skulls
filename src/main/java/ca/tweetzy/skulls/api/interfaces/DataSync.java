b83746772a89c924f1cc45f4d9f11006ebff92d9
Kiran Hart <kiran.hart@live.com>
2022-04-29 12:05:51 -0400

    create datasync interface
    Took 9 seconds

---
+++ src/main/java/ca/tweetzy/skulls/api/interfaces/DataSync.java
@@ -0,0 +1,12 @@
+package ca.tweetzy.skulls.api.interfaces;
+
+/**
+ * Date Created: April 29 2022
+ * Time Created: 11:35 a.m.
+ *
+ * @author Kiran Hart
+ */
+public interface DataSync {
+
+	void sync();
+}
