70c544e028d9267b22b821c0847dc99f8640339a
Kiran Hart <kiran.hart@live.com>
2022-05-02 14:04:24 -0400

    category selector gui
    Took 42 minutes

---
+++ src/main/java/ca/tweetzy/skulls/guis/CategorySelectorGUI.java
@@ -0,0 +1,42 @@
+package ca.tweetzy.skulls.guis;
+
+import ca.tweetzy.rose.comp.enums.CompMaterial;
+import ca.tweetzy.rose.gui.events.GuiClickEvent;
+import ca.tweetzy.rose.gui.template.PagedGUI;
+import ca.tweetzy.rose.utils.QuickItem;
+import ca.tweetzy.skulls.Skulls;
+import ca.tweetzy.skulls.api.interfaces.Category;
+import ca.tweetzy.skulls.settings.Translation;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Consumer;
+
+/**
+ * Date Created: May 02 2022
+ * Time Created: 1:49 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class CategorySelectorGUI extends PagedGUI<Category> {
+
+	private final Consumer<Category> selected;
+
+	public CategorySelectorGUI(Consumer<Category> selected) {
+		super(null, Translation.GUI_CUSTOM_CATEGORY_SELECTOR_TITLE.getString(), 6, Skulls.getCategoryManager().getCustomCategories());
+		this.selected = selected;
+	}
+
+	@Override
+	protected ItemStack makeDisplayItem(Category category) {
+		return QuickItem.of(CompMaterial.WRITTEN_BOOK)
+				.lore(Translation.GUI_CUSTOM_CATEGORY_SELECTOR_ITEMS_CATEGORY_LORE.getList("category_size", category.getSkulls().size()))
+				.name(Translation.GUI_CUSTOM_CATEGORY_SELECTOR_ITEMS_CATEGORY_NAME.getString("category_name", category.getName()))
+				.hideTags(true)
+				.make();
+	}
+
+	@Override
+	protected void onClick(Category category, GuiClickEvent clickEvent) {
+		this.selected.accept(category);
+	}
+}
