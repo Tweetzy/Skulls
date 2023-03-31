086f71d22d54f7afc314975b38d3164ee6474808
Kiran Hart <kiran.hart@live.com>
2023-03-31 15:07:10 -0400

    🪲 Fixed issue with skulls not registering on block faces
    Took 26 minutes

--- src/main/java/ca/tweetzy/skulls/listeners/SkullBlockListener.java
+++ src/main/java/ca/tweetzy/skulls/listeners/SkullBlockListener.java
@@ -28,7 +28,6 @@ import ca.tweetzy.skulls.hooks.WorldGuardHook;
 import ca.tweetzy.skulls.impl.PlacedSkullLocation;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -48,13 +47,16 @@ public final class SkullBlockListener implements Listener {
 		}
 
 		final ItemStack item = PlayerUtil.getHand(event.getPlayer());
-		if (!NBTEditor.contains(item, "Skulls:ID")) return;
+		if (!NBTEditor.contains(item, "Skulls:ID")) {
+			return;
+		}
 
 		final Block block = event.getBlockPlaced();
-		if (block.getType() != CompMaterial.PLAYER_HEAD.parseMaterial()) return;
+		if (block.getType() == CompMaterial.PLAYER_HEAD.parseMaterial() || block.getType() == CompMaterial.PLAYER_WALL_HEAD.parseMaterial()) {
+			final int skullId = Integer.parseInt(NBTEditor.getString(item, "Skulls:ID"));
+			Skulls.getSkullManager().addPlacedSkull(new PlacedSkullLocation(UUID.randomUUID(), skullId, block.getLocation()));
+		}
 
-		final int skullId = Integer.parseInt(NBTEditor.getString(item, "Skulls:ID"));
-		Skulls.getSkullManager().addPlacedSkull(new PlacedSkullLocation(UUID.randomUUID(), skullId, block.getLocation()));
 	}
 
 	@EventHandler()
@@ -66,16 +68,17 @@ public final class SkullBlockListener implements Listener {
 		}
 
 		final Block block = event.getBlock();
-		if (block.getType() != CompMaterial.PLAYER_HEAD.parseMaterial()) return;
 
-		if (!Skulls.getSkullManager().getPlacedSkulls().containsKey(block.getLocation())) return;
+		if (block.getType() == CompMaterial.PLAYER_HEAD.parseMaterial() || block.getType() == CompMaterial.PLAYER_WALL_HEAD.parseMaterial()) {
+			if (!Skulls.getSkullManager().getPlacedSkulls().containsKey(block.getLocation())) return;
 
-		final PlacedSkull placedSkull = Skulls.getSkullManager().getPlacedSkulls().get(block.getLocation());
-		Skulls.getSkullManager().removePlacedSkull(placedSkull);
+			final PlacedSkull placedSkull = Skulls.getSkullManager().getPlacedSkulls().get(block.getLocation());
+			Skulls.getSkullManager().removePlacedSkull(placedSkull);
 
-		event.setDropItems(false);
-		final Skull skull = Skulls.getSkullManager().getSkull(placedSkull.getSkullId());
-		block.getWorld().dropItemNaturally(block.getLocation(), skull.getItemStack());
+			event.setDropItems(false);
+			final Skull skull = Skulls.getSkullManager().getSkull(placedSkull.getSkullId());
+			block.getWorld().dropItemNaturally(block.getLocation(), skull.getItemStack());
+		}
 
 	}
 }
