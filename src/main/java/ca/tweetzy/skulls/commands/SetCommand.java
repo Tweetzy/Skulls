f7c491b1b6c3fdaa14c8c97207c27abbd81654e3
Kiran Hart <kiran.hart@live.com>
2023-07-02 18:39:59 -0400

    3.10.3
    Took 7 minutes

---
+++ src/main/java/ca/tweetzy/skulls/commands/SetCommand.java
@@ -0,0 +1,59 @@
+/*
+ * Skulls
+ * Copyright 2023 Kiran Hart
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ca.tweetzy.skulls.commands;
+
+import ca.tweetzy.flight.command.AllowedExecutor;
+import ca.tweetzy.flight.command.Command;
+import ca.tweetzy.flight.command.ReturnType;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public final class SetCommand extends Command {
+
+	public SetCommand() {
+		super(AllowedExecutor.PLAYER, "set");
+	}
+
+	@Override
+	protected ReturnType execute(CommandSender sender, String... args) {
+
+		return ReturnType.SUCCESS;
+	}
+
+	@Override
+	protected List<String> tab(CommandSender sender, String... args) {
+		return null;
+	}
+
+	@Override
+	public String getPermissionNode() {
+		return null;
+	}
+
+	@Override
+	public String getSyntax() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+}
